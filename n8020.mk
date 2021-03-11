f1e53a743edc75ca6b3f6ab733a00402380cd332
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-03-11 13:42:46 +0100

    Copy fstab into ramdisk and copy include init.target.usb.rc

--- n8020.mk
+++ n8020.mk
@@ -22,8 +22,11 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Init files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.smdk4x12:root/fstab.smdk4x12 \
+    $(LOCAL_PATH)/rootdir/fstab.smdk4x12:$(TARGET_COPY_OUT_RAMDISK)/fstab.smdk4x12 \
     $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc 
+    $(LOCAL_PATH)/rootdir/init.target.usb.rc:root/init.target.usb.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
