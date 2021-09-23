a47f930b2ee01e6fad35ead4743af3a55b8f3e77
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-09-23 14:15:18 +0200

    lineage_n8020.mk: fix inherit logic.

--- lineage_n8020.mk
+++ lineage_n8020.mk
@@ -2,10 +2,10 @@
 PRODUCT_RELEASE_NAME := n8020
 
 # Inherit device configuration
+$(call inherit-product, device/samsung/n8020/n8020.mk)
 $(call inherit-product, device/samsung/n80xx-common/n80xx-common.mk)
 $(call inherit-product, device/samsung/smdk4412-common/common.mk)
 $(call inherit-product, device/samsung/smdk4412-qcom-common/common.mk)
-$(call inherit-product, device/samsung/n8020/n8020.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
